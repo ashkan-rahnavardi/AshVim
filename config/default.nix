ba0057ca514d1b00c59f7de1d5f98ef2a1cef4d9
ash <ashkan.rahnavardi@gmail.com>
2024-10-08 09:39:35 -0700

    add tmux navigator

--- config/default.nix
+++ config/default.nix
@@ -47,6 +47,7 @@ _: {
 
     # Utils
     ./plugins/utils/telescope.nix
+    ./plugins/utils/tmux-navigator.nix
     ./plugins/utils/whichkey.nix
     ./plugins/utils/web-devicons.nix
     ./plugins/utils/mini.nix
