eb9a0cf7144a158f1ce6f0a312379ca9443897cb
ash <ashkan.rahnavardi@gmail.com>
2024-12-29 10:52:31 -0800

    remove custom ansible pkg

--- config/iac/language.nix
+++ config/iac/language.nix
@@ -18,16 +18,16 @@
     };
   };
 
-  extraPlugins = [
-    # Set ansible.yaml files
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "nvim-ansible";
-      src = pkgs.fetchFromGitHub {
-        owner = "mfussenegger";
-        repo = "nvim-ansible";
-        rev = "9c3b4a771b8c8d7b4f2171466464d978cb3846f7";
-        hash = "sha256-N1yOL77WpP66A2zrKxi7IOO5uRzAaeGa0Y/yWaIFiws=";
-      };
-    })
-  ];
+  # extraPlugins = [
+  #   # Set ansible.yaml files
+  #   (pkgs.vimUtils.buildVimPlugin {
+  #     name = "nvim-ansible";
+  #     src = pkgs.fetchFromGitHub {
+  #       owner = "mfussenegger";
+  #       repo = "nvim-ansible";
+  #       rev = "9c3b4a771b8c8d7b4f2171466464d978cb3846f7";
+  #       hash = "sha256-N1yOL77WpP66A2zrKxi7IOO5uRzAaeGa0Y/yWaIFiws=";
+  #     };
+  #   })
+  # ];
 }
