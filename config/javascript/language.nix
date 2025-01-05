3652d36822fb4a192d7c53454cce10fc688642b8
ash <ashkan.rahnavardi@gmail.com>
2025-01-04 16:20:01 -0800

    remove async autosave

--- config/javascript/language.nix
+++ config/javascript/language.nix
@@ -71,7 +71,7 @@
           vim.api.nvim_create_autocmd('BufWritePre', {
             buffer = bufnr,
             callback = function()
-              vim.lsp.buf.format({ async = true })
+              vim.lsp.buf.format({ async = false })
             end,
           })
         '';
