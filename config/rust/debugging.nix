1b9541436d36c292f900d14f3362d8f492370438
ash <ashkan.rahnavardi@gmail.com>
2025-03-04 10:29:30 -0800

    fix rust rename

--- config/rust/debugging.nix
+++ config/rust/debugging.nix
@@ -1,19 +1,23 @@
 {pkgs, ...}: {
+  plugins = {
+    dap = {
+      enable = true;
+      adapters = {
+        executables = {lldb = {command = "${pkgs.lldb_19}/bin/lldb-dap";};};
+      };
+    };
+    dap-virtual-text.enable = true;
+    dap-ui.enable = true;
+  };
   # plugins.dap = {
   #   enable = true;
   #   extensions = {
   #     dap-ui.enable = true;
   #     dap-virtual-text.enable = true;
   #   };
-
-  plugins = {
-    dap.enable = true;
-    dap-virtual-text.enable = true;
-    dap-ui.enable = true;
-  };
-
-  adapters = {
-    executables = {lldb = {command = "${pkgs.lldb_19}/bin/lldb-dap";};};
-  };
+  #
+  #   adapters = {
+  #     executables = {lldb = {command = "${pkgs.lldb_19}/bin/lldb-dap";};};
+  #   };
   # };
 }
