106c29f88f10d4f6de78127523b4ffd5aab6c832
Ashkan Rahnavardi <ashkan.rahnavardi@gmail.com>
2025-06-17 09:42:12 -0700

    update augment code

--- config/plugins.nix
+++ config/plugins.nix
@@ -14,8 +14,10 @@
     src = pkgs.fetchFromGitHub {
       owner = "augmentcode";
       repo = "augment.vim";
-      rev = "e14cbe9fdc33dff1a4b55c0fc858b94f5e833862";
-      hash = "sha256-VzHFmt8uOsZEYqIGAbwSrmGLlOfTnL7QvsRB5yMpSG0=";
+      rev = "df56563fe534eb7d0daeaa8c8de3b54700fbbaf8";
+      hash = "sha256-aq7W1GoJNP59Gzx9eOFA0CLz5S+mUh3DSP4izmr2E0g=";
+      # rev = "e14cbe9fdc33dff1a4b55c0fc858b94f5e833862";
+      # hash = "sha256-VzHFmt8uOsZEYqIGAbwSrmGLlOfTnL7QvsRB5yMpSG0=";
     };
     nvimSkipModule = "outline.providers.norg";
 
