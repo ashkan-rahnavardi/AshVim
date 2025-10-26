8da5f254bad09ae43996abe117aca4e724bd3069
Ashkan Rahnavardi <ashkan.rahnavardi@gmail.com>
2025-10-26 13:50:52 -0700

    remove

--- config/debugging.nix
+++ config/debugging.nix
@@ -14,13 +14,6 @@ lib.mkMerge [
       cmp-dap.enable = true;
       dap = {
         enable = true;
-        extensions = {
-          # dap-ui = {
-          #   enable = true;
-          #   floating.mappings = {close = ["<ESC>" "q"];};
-          # };
-          # dap-virtual-text = {enable = true;};
-        };
         signs = {
           dapBreakpoint = {
             text = "";
